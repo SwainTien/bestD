256875200b6654c47c17c9e40944bb8405820f0a
SwainTien <tnsh007@qq.com>
2017-09-09 18:26:31 +0800

    新的冲突

--- project-test/src/test/JavaFirst.java
+++ project-test/src/test/JavaFirst.java
@@ -6,10 +6,11 @@ public class JavaFirst {
 
 	@Test
 	public void testHelloWrold() {
-		System.out.println("HelloWorld!");
+		
+
 		System.out.println("HelloWorld!");
 
-System.out.println("Hello");
+
 System.out.println(" ");
 System.out.println("World!");
 
