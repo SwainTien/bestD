8e04474e5ad0a8406bb2c8cba12e01aefadc9c47
SwainTien <tnsh007@qq.com>
2017-09-09 18:17:10 +0800

    测试冲突

--- project-test/src/test/JavaFirst.java
+++ project-test/src/test/JavaFirst.java
@@ -7,5 +7,11 @@ public class JavaFirst {
 	@Test
 	public void testHelloWrold() {
 		System.out.println("HelloWorld!");
+
+System.out.println("Hello");
+System.out.println(" ");
+System.out.println("World!");
+
+
 	}
 }
